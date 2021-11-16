182de50d174cb600f2a9c3d074096e94d3a61b16
amalianurulrahmawati <92409385+amalianurulrahmawati@users.noreply.github.com>
2021-11-16 08:02:58 +0700

    Add files via upload

---
+++ 2117051008_Amalia Nurul Rahmawati_postest2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	
+	int n,a,b;
+	n = 31;
+	a = 8/2;
+	b = 5*4;
+	
+	//input
+	cout<< n <<endl;
+	cout<< a <<endl;
+	cout<< b <<endl;
+	
+	//output
+	cout<<"Jumlam Bilangan Hingga Suku Ke-"<< n <<" Dari Barisan Aritmatika Ini Adalah Hasil "<< 640 <<endl;
+	
+	return 0;
+
+}
